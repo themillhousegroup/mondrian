b884f200bbc3d856e1d573b88b6ae6402f9fc0cb
John Marshall <john@themillhousegroup.com>
2016-01-15 08:19:29 +1100

    Documenting some classes and fields

--- src/main/scala/com/themillhousegroup/mondrian/MongoEntity.scala
+++ src/main/scala/com/themillhousegroup/mondrian/MongoEntity.scala
@@ -1,5 +1,9 @@
 package com.themillhousegroup.mondrian
 
+/** Domain objects to be persisted in a MongoDB database should extend
+  * this trait, which defines the `_id` field as required by Mongo
+  * and provides a convenient `id` accessor to read it as a String
+  */
 trait MongoEntity {
   val _id: Option[MongoId]
   val id: String = _id.fold("NOT_SAVED_YET")(_.$oid)
--- src/main/scala/com/themillhousegroup/mondrian/MongoId.scala
+++ src/main/scala/com/themillhousegroup/mondrian/MongoId.scala
@@ -2,8 +2,10 @@ package com.themillhousegroup.mondrian
 
 import play.api.libs.json.Json
 
+/** Represents the particular format of the Mongo "primary key", the `$oid` */
 case class MongoId(val $oid: String)
 
+/** Provides a Play JSON `Format` for working with MongoId instances */
 object MongoJson {
   implicit val mongoIdFormat = Json.format[MongoId]
 }
