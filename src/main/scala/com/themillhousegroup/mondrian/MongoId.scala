1e313556778794648834708287c57ee1340a802b
John Marshall <john@bridgeokay.com>
2016-05-31 12:06:19 +1000

    Helper methods as per the Mongo documentation for ObjectId

--- src/main/scala/com/themillhousegroup/mondrian/MongoId.scala
+++ src/main/scala/com/themillhousegroup/mondrian/MongoId.scala
@@ -1,6 +1,29 @@
 package com.themillhousegroup.mondrian
 
+import org.apache.commons.lang3.StringUtils
 import play.api.libs.json.Json
+
+
+/** See https://docs.mongodb.com/manual/reference/method/ObjectId/ */
+object MongoId {
+  lazy val objectIdRegex = """^[a-f\d]{24}$""".r
+
+  def withValidId(candidate:String):Option[String] = {
+    val maybeId = Option(StringUtils.trimToNull(candidate))
+
+    maybeId.flatMap { id =>
+      objectIdRegex.findFirstIn(id)
+    }
+  }
+
+  def isValid(candidate:String):Boolean = withValidId(candidate).nonEmpty
+
+  def timestamp(candidate:String):Option[Long] = {
+    withValidId(candidate).map { oid =>
+      oid.substring(0, 7).toLong
+    }
+  }
+}
 
 /** Represents the particular format of the Mongo "primary key", the `$oid` */
 case class MongoId(val $oid: String)
